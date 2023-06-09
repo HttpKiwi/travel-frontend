4f99d6a4a75866a9f0a8640c6b831a9902b43db2
Juan Pablo Dominguez <juan.avenia@correounivalle.edu.co>
2023-06-09 14:00:49 -0500

    updating fetch route

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
     const [completion, setCompletion] = useState("")
 
     const getCompletion = async () => {
-        const res = await fetch(`https://143.198.177.199/:5000/"${value}"`)
+        const res = await fetch(`https://spectratravel.herokuapp.com/"${value}"`)
         setCompletion(await res.text())
     }
 
